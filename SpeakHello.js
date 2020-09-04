faa5382912e3be8ce5ebb63befa70889c50cd649
cmarkpatosa <pogz.m17@gmail.com>
2020-09-04 13:33:03 +0800

    Add files via upload

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,10 +1,6 @@
-// Variation 1
-(function(
-) {  
-    // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-    // See Lecture 52, part 2
-
-
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+(function () {
     // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
     // the "speak" method and which you will expose to the global context
     // See Lecture 52, part 1
@@ -17,15 +13,12 @@
     // helloSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
     helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
+        console.log(speakWord + " " + name);
     }
-    
 
     // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
     // 'helloSpeaker' on the global scope as well.
-    helloSpeaker.speak('John');
-  
     // See Lecture 52, part 2
     // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-    // xxxx.xxxx = helloSpeaker;
-}());
+    window.helloSpeaker = helloSpeaker;
+})();
--- script.js
+++ script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function() { // 1
+(function () {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     // STEP 10:
@@ -41,26 +41,28 @@ WARNING!!! WARNING!!!
     // using the 'speak' method or either helloSpeaker's or byeSpeaker's
     // 'speak' method.
     // See Lecture 50, part 1
-    for (i=0;i<= names.length; i++/* fill in parts of the 'for' loop to loop over names array */) {
+    for (var i = 0; i <= names.length-1; i++ /* fill in parts of the 'for' loop to loop over names array */) {
 
-      // STEP 11:
-      // Retrieve the first letter of the current name in the loop.
-      // Use the string object's 'charAt' function. Since we are looking for
-      // names that start with either upper case or lower case 'J'/'j', call
-      // string object's 'toLowerCase' method on the result so we can compare
-      // to lower case character 'j' afterwards.
-      // Look up these methods on Mozilla Developer Network web site if needed.
-      var firstLetter = names[i].charAt(0);
-alert(firstLetter)
-      // STEP 12:
-      // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-      // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-      // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-      // name in the loop.
-      if (firstLetter === 'j') {
-        // byeSpeaker.xxxx
-      } else {
-        // helloSpeaker.xxxx
-      }
-    }     
-}());     
+        // STEP 11:
+        // Retrieve the first letter of the current name in the loop.
+        // Use the string object's 'charAt' function. Since we are looking for
+        // names that start with either upper case or lower case 'J'/'j', call
+        // string object's 'toLowerCase' method on the result so we can compare
+        // to lower case character 'j' afterwards.
+        // Look up these methods on Mozilla Developer Network web site if needed.
+        var firstLetter = names[i].charAt(0);
+
+        // STEP 12:
+        // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+        // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+        // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+        // name in the loop.
+        if (firstLetter.toLowerCase() =='j') {
+            // byeSpeaker.xxxx
+            byeSpeaker.speak(names[i]);
+        } else {
+            // helloSpeaker.xxxx
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
