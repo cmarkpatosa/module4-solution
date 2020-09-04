359fbed84595b62e596a2a22907cf7c52e198763
cmarkpatosa <pogz.m17@gmail.com>
2020-09-04 13:35:11 +0800

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -17,6 +17,7 @@
     // See Lecture 52, part 2
     byeSpeaker.speak = function (name) {
         console.log(speakWord + " " + name);
+        document.write(speakWord + " " + name);
     }
 
     // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
